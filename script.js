b85f6b558d586b26fd489cd76e9030b96aadd45f
Vish2503 <visheshakriti@gmail.com>
2023-07-21 20:22:59 +0530

    3x3 cube made with one side moving

--- script.js
+++ script.js
@@ -5,19 +5,32 @@ const scene = new THREE.Scene();
 
 // Create a red cube
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'red'});
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
-scene.add(cubeMesh);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'white'});
+
+let cubeMesh = []
+for (let i = 0; i < 3; i++) {
+    cubeMesh[i] = []
+    for (let j = 0; j < 3; j++) {
+        cubeMesh[i][j] = []
+        for (let k = 0; k < 3; k++) {
+            cubeMesh[i][j][k] = new THREE.Mesh(cubeGeometry, cubeMaterial);
+            cubeMesh[i][j][k].position.set(i-1, j-1, k-1)
+            scene.add(cubeMesh[i][j][k]);
+        } 
+    }
+}
+
 
 // Sizes to give aspect ratio in camera
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 };
 
 // Add a camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
+camera.position.set(-5,5,5);
+camera.lookAt(new THREE.Vector3())
 scene.add(camera);
 
 // Create renderer
@@ -27,4 +40,30 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 
-renderer.render(scene, camera);
+// Try animation
+let time = Date.now()
+let i = 0
+const animate = () => {
+
+    const currentTime = Date.now()
+    const dt = currentTime - time
+    time = currentTime
+
+    // console.log(cubeMesh.position.angleTo(new THREE.Vector3(0, 0, 0)))
+    // console.log(cubeMesh.position.applyMatrix3(new THREE.Matrix3(0.002*dt, 0, 0, 0, 0.002*dt, 0, 0, 0, 1)))
+    let m = new THREE.Matrix4(); 
+    m.makeRotationZ(0.002 * dt)
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            cubeMesh[i][j][2].position.applyMatrix4(m)
+            cubeMesh[i][j][2].rotateZ(0.002*dt)
+        }
+    }
+    
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(animate)
+}
+
+animate()
