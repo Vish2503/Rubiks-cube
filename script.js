4dc505cb382f1ff539d902278839f33bae4218b9
Vish2503 <visheshakriti@gmail.com>
2023-07-21 17:43:28 +0530

    Using Vite now

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
+import * as THREE from 'three';
+
 // Create a Scene
-const scene = new THREE.Scene() 
+const scene = new THREE.Scene();
 
 // Create a red cube
-const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'red'})
-const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
-scene.add(cubeMesh)
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 'red'});
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+scene.add(cubeMesh);
 
 // Sizes to give aspect ratio in camera
 const sizes = {
     width: 800,
     height: 600
-}
+};
 
 // Add a camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Create renderer
-const canvas = document.querySelector('.webgl') 
+const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
+});
+renderer.setSize(sizes.width, sizes.height);
 
-renderer.render(scene, camera)
+renderer.render(scene, camera);
